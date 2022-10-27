13109cad760286c15ce5679afeb2159d6768093f
Murray Patterson <murray.patterson@gmail.com>
2022-10-27 12:01:39 -0400

    simplify find

--- find.py
+++ find.py
@@ -10,24 +10,15 @@ import sys, re
 
   gives:
 
-    1 ['hi', 'ho']
-
-  while,
-
-    python find.py fullmatch "[a-z]+" hi.txt split one-per-line
-
-  gives:
-
-    1 ['how']
-    1 ['are']
+    hi ho
 
   to be used with caution (i.e., it is not "automagic" :)
 
 '''
 
 #
-# using a particular `searchtype` (search, match, fullmatch, findall,
-# etc.), look for `pattern` in `string`
+# using a particular "searchtype" (search, match, fullmatch, findall,
+# etc.), look for "pattern" in "string"
 def find(searchtype, pattern, string) :
 
     # make all groups non-capturing by default
@@ -43,39 +34,15 @@ def find(searchtype, pattern, string) :
 #----------------------------------------------------------------------
 
 searchname, pattern, filename = sys.argv[1:4] # first 3 arguments
-
-processing = None
-if len(sys.argv) > 4 : # optional 4th argument
-    processing = sys.argv[4]
-
-perline = 'many'
-if len(sys.argv) > 5 : # optional 5th argument
-    perline = sys.argv[5]
-
 searchtype = getattr(re, searchname)
-lines = open(filename, encoding = 'utf8', errors = 'ignore')
 
-count = 0
+lines = sys.stdin
+if filename != '-' :
+    lines = open(filename, encoding = 'utf8', errors = 'ignore')
+
 for line in lines :
-    count += 1
+    line = line.strip()
 
-    if processing == 'strip' :
-        line = line.strip()
-
-    match = find(searchtype, pattern, line) # "default"
-
-    if processing == 'split' :
-        match = []
-
-        for token in line.split() :
-            m = find(searchtype, pattern, token)
-            match += m if m else []
-
+    match = find(searchtype, pattern, line)
     if match :
-
-        if perline == 'one-per-line' :
-            for m in match :
-                print(count, [m])
-
-        else :
-            print(count, match)
+        print(' '.join(match))
